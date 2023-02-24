ff3cd38194bd3bd6e4ed1d9528bc4393cf7cf4e6
MohamedFola <99367479+MohamedFola@users.noreply.github.com>
2023-02-24 16:21:40 +0200

    adding list comprehensions and captalize problems

---
+++ Capitalize/code_1.py
@@ -0,0 +1,41 @@
+# Capitalize Problem
+# st = "hello world"
+
+# ch = st.split()
+
+# # print(ch)
+# for i, _ in enumerate(ch):
+#     ch[i] = _.capitalize()
+#     print(_.capitalize())
+
+# print(ch)
+# st = " ".join(ch)
+# print(st)
+# ------------------------------------------- #
+
+# String Validators
+
+# if __name__ == '__main__':
+#     s = input()
+#     b = [False, False, False, False, False]
+
+#     for i in range(len(s)):
+#         if s[i].isalnum():
+#             b[0] = True
+
+#         if s[i].isalpha():
+#             b[1] = True
+
+#         if s[i].isdigit():
+#             b[2] = True
+
+#         if s[i].islower():
+#             b[3] = True
+
+#         if s[i].isupper():
+#             b[4] = True
+
+# for i in b:
+#     print(i)
+
+# ------------------------------------------- #
